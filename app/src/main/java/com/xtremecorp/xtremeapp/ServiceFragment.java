116f16d8080a12fe654456d516be0e78b1f7d1be
AlexisVil <alexisvilchiz@gmail.com>
2022-07-28 17:45:14 -0500

    Switch case

--- app/src/main/java/com/xtremecorp/xtremeapp/ServiceFragment.java
+++ app/src/main/java/com/xtremecorp/xtremeapp/ServiceFragment.java
@@ -7,13 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ServiceFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ServiceFragment extends Fragment {
+public class ServiceFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,10 +60,94 @@ public class ServiceFragment extends Fragment {
         }
     }
 
+    ImageButton TelmexBtn, InfonavitBtn, NaturaBtn, CfeBtn, SkyBtn,
+            IzziBtn, TotalPlayBtn, DishBtn, JafraBtn, AvonBtn,
+            PaseBtn, NaturgyBtn;
+    private List<ImageButton> buttons;
+    private static final int[] Buttons_ID = {
+            R.id.telmex,     //0
+            R.id.infonavit,  //1
+            R.id.natura,     //2
+            R.id.cfe,        //3
+            R.id.sky,        //4
+            R.id.izzi,       //5
+            R.id.totalplay,  //6
+            R.id.dish,       //7
+            R.id.jafra,      //8
+            R.id.avon,       //9
+            R.id.pase,       //10
+            R.id.naturgy     //11
+    };
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_service, container, false);
+
+        buttons = new ArrayList<ImageButton>();
+        View root = inflater.inflate(R.layout.fragment_service, container, false);
+
+        for(int id : Buttons_ID){
+            ImageButton imageButton = root.findViewById(id);
+            imageButton.setOnClickListener(this);
+            buttons.add(imageButton);
+        }
+
+        TelmexBtn = buttons.get(0);
+        InfonavitBtn = buttons.get(1);
+        NaturaBtn = buttons.get(2);
+        CfeBtn = buttons.get(3);
+        SkyBtn = buttons.get(4);
+        IzziBtn = buttons.get(5);
+        TotalPlayBtn = buttons.get(6);
+        DishBtn = buttons.get(7);
+        JafraBtn = buttons.get(8);
+        AvonBtn = buttons.get(9);
+        PaseBtn = buttons.get(10);
+        NaturgyBtn = buttons.get(11);
+        return root;
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.telmex:
+                Toast.makeText(getContext(), "Telmex", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.infonavit:
+                Toast.makeText(getContext(), "Infonavit", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.natura:
+                Toast.makeText(getContext(), "Natura", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cfe:
+                Toast.makeText(getContext(), "Cfe", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.sky:
+                Toast.makeText(getContext(), "SKY", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.izzi:
+                Toast.makeText(getContext(), "IZZI", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.totalplay:
+                Toast.makeText(getContext(), "Total Play", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dish:
+                Toast.makeText(getContext(), "Dish", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.jafra:
+                Toast.makeText(getContext(), "Jafra", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.avon:
+                Toast.makeText(getContext(), "Avon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.pase:
+                Toast.makeText(getContext(), "Telepase", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.naturgy:
+                Toast.makeText(getContext(), "Naturgy", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
--- app/src/main/java/com/xtremecorp/xtremeapp/TaeFragment.java
+++ app/src/main/java/com/xtremecorp/xtremeapp/TaeFragment.java
@@ -11,12 +11,15 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link TaeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TaeFragment extends Fragment {
+public class TaeFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,32 +61,107 @@ public class TaeFragment extends Fragment {
         }
     }
 
-    ImageButton TelcelBtn, MovistarBtn, VirginBtn, AttBtn, BaitBtn;
+    ImageButton TelcelBtn, MovistarBtn, VirginBtn, AttBtn, BaitBtn,
+            DiriBtn, JrMovilBtn, MayaMovilBtn, MiMovilBtn, OuiBtn,
+            PilloFonBtn, SpaceBtn, ValorBTn, DiveracyBtn;
+
+    private List<ImageButton> buttons;
+    private static final int[] Buttons_Id = {
+            R.id.btn_telcel,//0
+            R.id.movistar, //1
+            R.id.att,      //2
+            R.id.virgin,   //3
+            R.id.bait,     //4
+            R.id.diri,     //5
+            R.id.jrmovil,  //6
+            R.id.maya,     //7
+            R.id.mimovil,  //8
+            R.id.oui,      //9
+            R.id.pillofon, //10
+            R.id.space,    //11
+            R.id.valor,    //12
+            R.id.diveracy  //13
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //Crear un areray list y llenar dinamicamente
+        buttons = new ArrayList<ImageButton>();
+
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_tae, container, false);
-        TelcelBtn = root.findViewById(R.id.btn_telcel);
-        MovistarBtn = root.findViewById(R.id.movistar);
-        VirginBtn = root.findViewById(R.id.virgin);
-        AttBtn = root.findViewById(R.id.att);
-        BaitBtn = root.findViewById(R.id.bait);
 
-        productSelection(TelcelBtn);
-        productSelection(MovistarBtn);
+        for (int id : Buttons_Id) {
+            ImageButton imageButton = root.findViewById(id);
+            imageButton.setOnClickListener(this);
+            buttons.add(imageButton);
+        }
 
-
+        TelcelBtn = buttons.get(0);
+        MovistarBtn = buttons.get(1);
+        AttBtn = buttons.get(2);
+        VirginBtn = buttons.get(3);
+        BaitBtn = buttons.get(4);
+        DiriBtn = buttons.get(5);
+        JrMovilBtn = buttons.get(6);
+        MayaMovilBtn = buttons.get(7);
+        MiMovilBtn = buttons.get(8);
+        OuiBtn = buttons.get(9);
+        PilloFonBtn = buttons.get(10);
+        SpaceBtn = buttons.get(11);
+        ValorBTn = buttons.get(12);
+        DiveracyBtn = buttons.get(13);
 
         return root;
     }
 
-    public void productSelection(ImageButton imageButton){
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getContext(), "boton " , Toast.LENGTH_SHORT).show();
-            }
-        });
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_telcel:
+                Toast.makeText(getContext(), "Telcel", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.movistar:
+                Toast.makeText(getContext(), "Movistar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.att:
+                Toast.makeText(getContext(), "Att", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.virgin:
+                Toast.makeText(getContext(), "virgin", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bait:
+                Toast.makeText(getContext(), "Bait", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.diri:
+                Toast.makeText(getContext(), "Diri", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.jrmovil:
+                Toast.makeText(getContext(), "jrMovil", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.maya:
+                Toast.makeText(getContext(), "Maya Movil", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mimovil:
+                Toast.makeText(getContext(), "Mi movil", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.oui:
+                Toast.makeText(getContext(), "Oui Movil", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.pillofon:
+                Toast.makeText(getContext(), "Pillofon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.space:
+                Toast.makeText(getContext(), "Space Movil", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.diveracy:
+                Toast.makeText(getContext(), "Diveracy", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.valor:
+                Toast.makeText(getContext(), "Valor Movil", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
